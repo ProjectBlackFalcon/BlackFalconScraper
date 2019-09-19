48817a6c3efdb3e8f251b5ec522db832fc19936f
Ugdha <alexisvlrt@gmail.com>
2019-09-19 11:21:23 +0200

    Back to the regular schedule

--- Scraper.py
+++ Scraper.py
@@ -79,7 +79,7 @@ class Scraper:
                     print('Trying again in 5 minutes')
                     time.sleep(5 * 60)
 
-            if hour == 10 and datetime.datetime.now().hour == 11:
+            if hour == 22 and datetime.datetime.now().hour == 23:
                 DiscordMessageSender(f'Today the scraper ran {scraping_rounds} times and was successful {successful} times. It added {scraped_today} entries to the database').run(credentials['discord']['token'])
             hour = datetime.datetime.now().hour
 
